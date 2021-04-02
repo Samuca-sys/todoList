8f3a7126d29ca736a8bdbd61509faa1921965a26
Samuel Macedo <samuel.macfer@gmail.com>
2021-04-02 17:06:55 -0300

    ci: add initial todo app structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 export default function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="container">
+        <div className="content">
+          <header >
+            <h1>THINGS TO DO</h1>
+          </header>
+
+          <section>
+            <ul className="list">
+              <li>
+                <label>
+                  <input type="checkbox" value=""/> Take ou the trash
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input type="checkbox" value=""/> Buy bread
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input type="checkbox" value=""/> Teach penguins to fly
+                </label>
+              </li>
+            </ul>
+        </section>
+      </div>
+    </div>  
   );
 }
