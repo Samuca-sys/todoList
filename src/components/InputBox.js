1638c6c1e12cf8ceec0cac2139bab534e27a1234
Samuel Macedo <samuel.macfer@gmail.com>
2021-04-03 11:46:38 -0300

    ci: add keycode-js form KeyCode constants

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import * as KeyCode from 'keycode-js';
 
-export default class InputBox extends React.Component {
+class InputBox extends React.Component {
   constructor (props) {
     super(props)
     this.state = { 
       value: props.value || "" //delare value state, default value is ""
     }
-    //binding this to method handleInputBoxChange in the constructor
-  
   }
   handleInputBoxChange(event) {
     //it will receive value from value property in real time
@@ -15,6 +14,14 @@ export default class InputBox extends React.Component {
       {value: event.target.value}
     )
   }
+
+  handleOnKeyUp(event) {
+    if(event.keyCode === KeyCode.KEY_RETURN){//if pressed return key
+      //Add new TodoItem
+      //Clear the text box
+    }
+  }
+
   render () {
     return (
     <input 
@@ -22,9 +29,11 @@ export default class InputBox extends React.Component {
       placeholder="Add New" 
       className="add-todo"
       value={this.state.value} //value property will receive value state
-      //binding this to method handleInputBoxChange in onChange pproperty
-      onChange={this.handleInputBoxChange.bind(this)}/>
+      //binding this to method handleInputBoxChange in onChange property
+      onChange={this.handleInputBoxChange.bind(this)}
+      onKeyUp={this.handleOnKeyUp.bind(this)}/>
     )
   }
 }
 
+export default InputBox;
