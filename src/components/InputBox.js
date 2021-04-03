6a4aacfb25b7bbf6d227bfbaf6974570127d88bc
Samuel Macedo <samuel.macfer@gmail.com>
2021-04-03 11:17:19 -0300

    ci: convert InputBox.js to a stateful component

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,5 +1,30 @@
-export default function InputBox() {
-  return (
-    <input type="text" placeholder="Add New" className="add-todo"/>
-  )
+import React from 'react';
+
+export default class InputBox extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { 
+      value: props.value || "" //delare value state, default value is ""
+    }
+    //binding this to method handleInputBoxChange in the constructor
+  
+  }
+  handleInputBoxChange(event) {
+    //it will receive value from value property in real time
+    this.setState(
+      {value: event.target.value}
+    )
+  }
+  render () {
+    return (
+    <input 
+      type="text"   
+      placeholder="Add New" 
+      className="add-todo"
+      value={this.state.value} //value property will receive value state
+      //binding this to method handleInputBoxChange in onChange pproperty
+      onChange={this.handleInputBoxChange.bind(this)}/>
+    )
+  }
 }
+
