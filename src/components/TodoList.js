bd645fa3811fad3af6865e2e2725c346d12939e1
Samuel Macedo <samuel.macfer@gmail.com>
2021-04-05 16:40:24 -0300

    refactor: extract a new FilteredList component from TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,30 +1,17 @@
-import TodoItem from "./TodoItem";
+import FilteredList from "./FilteredList";
 
 import {applyFilter} from './../services/filter'
 
 export default function TodoList(props) {
   const {items, filter, handleOnChangeStatus} = props;
-  const filteredList = applyFilter(items, filter)
+  const filteredItems= applyFilter(items, filter)
 
   return (
-    <>
-      <section>
-        {filteredList.length > 0 
-          ? (
-            <ul className="item-list">
-              {/*render list dinamically using array items from App*/}
-              {filteredList.map(item => 
-                (<TodoItem 
-                  key={item.id} 
-                  item={item} 
-                  handleChangeStatus={handleOnChangeStatus}
-                />))}
-            </ul>
-        )
-        : (
-          <p>There are no items.</p>
-        )}
-      </section>
-    </>
+    <section>
+      <FilteredList 
+        items={filteredItems} 
+        handleOnChangeStatus={handleOnChangeStatus}
+      />
+    </section>
   )
 }
