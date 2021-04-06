f8551bd9c1f4f540793c8dcb944189ac73bb20dd
Samuel Macedo <samuel.macfer@gmail.com>
2021-04-06 18:33:29 -0300

    ci: add handleOnClickChangeMode

--- src/components/ButtonWrapper.js
+++ src/components/ButtonWrapper.js
@@ -1,21 +1,33 @@
 import addImg from './../assets/add.svg'
 import searchImg from './../assets/search.svg'
 
-import {MODE_CREATE, MODE_SEARCH} from './../services/mode'
+import {MODE_NONE, MODE_CREATE, MODE_SEARCH} from './../services/mode'
 
 export default function ButtonWrapper(props) {
-  const {mode} = props
+  const {handleOnClickChangeMode, mode} = props
+  const isCreateMode = () => mode === MODE_CREATE;
+  const isSearchMode = () => mode === MODE_SEARCH;
   return (
     <>
       <img 
         src={addImg} 
         alt="add signal" 
-        classname={(mode === MODE_CREATE ? 'selected': '')}
+        classname={(isCreateMode() ? 'selected': '')}
+        onClick = {
+          () => handleOnClickChangeMode(
+            isCreateMode() ? MODE_NONE : MODE_CREATE
+          )
+        }
       />
       <img 
         src={searchImg} 
         alt="search signal" 
-        classname={(mode === MODE_SEARCH ? 'selected': '')}
+        classname={(isSearchMode() ? 'selected': '')}
+        onClick = {
+          () => handleOnClickChangeMode(
+            isSearchMode() ? MODE_NONE : MODE_SEARCH
+          )
+        }
       />
     </>
   )
