b6accc230ac1dc2d06be1eebbb59d09ebcd038ea
Samuel Macedo <samuel.macfer@gmail.com>
2021-04-06 16:49:44 -0300

    ci: add icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
 import Filter from "./Filter"
 
+import addImg from './../assets/add.svg'
+import searchImg from './../assets/search.svg'
+
 export default function Footer(props) {
   const {count, filter, handleOnChangeFilter} = props
   return (
     <footer>
       <nav>
         <div>
+          <img src={addImg} alt="add signal" classname="addImg"/>
+          <img src={searchImg} alt="search signal" classname="searchImg"/>
           <strong>
           <span>{count} </span>
           </strong>
